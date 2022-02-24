fba81c8d3c60f7629f6be0f64ed9cb0dcdef7430
Victor Wåhlstrand Skärström <victor.wahlstrand@gmail.com>
2022-02-24 10:21:23 +0100

    Updated footer to better handling of small screens

--- src/components/Footer.vue
+++ src/components/Footer.vue
@@ -1,47 +1,39 @@
 <template>
-  <div id="foot" class="foot">
-    <div class="foot-container">
-      <div id="contacts" class="contacts">
-        <b>CONTACT US:</b><br />
-        Professor Johan Ling <br />
-        <a href="mailto:johan.ling@gu.se">johan.ling@gu.se</a><br />
-        Centre for Digital Humanities<br />
-        <a href="mailto:cdh@lir.gu.se">cdh@lir.gu.se</a>
-      </div>
-      <div class="attribution-row" id="osu">
-        <img class="image-attribution"  src="/assets/images/osu_long.png" />
-      </div>
-      <div class="attribution-row" id="shfa">
-        <img class="image-attribution" src="/assets/images/shfa_logo.png" />
-      </div>
-      <div class="attribution-row" id="rj">
-        <img class="image-attribution" src="/assets/images/RJ_logo_white.png"
-        />
-      </div>
-      <div class="attribution-row">
-        <CdhLogo id="cdh" />
-      </div>
-
-      <!-- <v-row no-gutters> 
-            <v-col align='left' justify='left' cols="2">
-
-            </v-col>
-            <v-col align="center" justify="center" cols="2">
-                <img height="75px" width="auto" src="/assets/images/osu_long.png">
-            </v-col>
-            <v-col align="center" justify="center" cols="2">
-                <img height="75px" width="auto" src="/assets/images/shfa_logo.png">
-            </v-col>
-            <v-col align="left" justify="center" cols="2">
-                <img height='100px' width="auto" src="/assets/images/RJ_logo_white.png">
-            </v-col>
-            <v-spacer></v-spacer>
-            <v-col align="right" justify="right" class="flex-grow-1 flex-shrink-0" cols="4">
-                <CdhLogo />
-            </v-col>
-          </v-row> -->
-    </div>
-  </div>
+  <v-container id="foot" class="foot py-12" fluid>
+    <v-row>
+      <v-col>
+        <div id="contacts" class="contacts">
+          <b>CONTACT US:</b><br />
+          Professor Johan Ling <br />
+          <a href="mailto:johan.ling@gu.se">johan.ling@gu.se</a><br />
+          Centre for Digital Humanities<br />
+          <a href="mailto:cdh@lir.gu.se">cdh@lir.gu.se</a>
+        </div>
+      </v-col>
+      <v-col>
+        <div class="attribution-row" id="osu">
+          <img class="image-attribution" src="/assets/images/osu_long.png" />
+        </div>
+      </v-col>
+      <v-col>
+        <div class="attribution-row" id="shfa">
+          <img class="image-attribution" src="/assets/images/shfa_logo.png" />
+        </div>
+      </v-col>
+      <v-col>
+        <div class="attribution-row" id="rj">
+          <img
+            class="image-attribution"
+            src="/assets/images/RJ_logo_white.png"
+          /></div
+      ></v-col>
+      <v-col>
+        <div class="attribution-row">
+          <CdhLogo id="cdh" />
+        </div>
+      </v-col>
+    </v-row>
+  </v-container>
 </template>
 
 <script>
@@ -55,40 +47,38 @@ export default {
 
 <style>
 .image-attribution {
-    align-self: left;
-    max-width: 300px;
-    max-height: 100px;
-    height: auto; 
-    margin-left: 30px;
-    margin-right: 30px;
-
+  align-self: left;
+  max-width: 300px;
+  max-height: 100px;
+  height: auto;
+  margin-left: 30px;
+  margin-right: 30px;
 }
-
-
 
 .contacts {
   font-size: 15px;
   /* width: 300px; */
   /* display: inline-block; */
   margin-right: 75px;
-      align-self: left;
-    max-width: 300px;
-    max-height: 100px;
-    height: auto; 
+  align-self: left;
+  max-width: 300px;
+  max-height: 100px;
+  height: auto;
 }
 
 #cdh {
-      align-self: left;
-    max-width: 300px;
-    max-height: 100px;
-    height: auto; 
-    margin-left: 30px;
-    margin-right: 30px;
+  align-self: left;
+  max-width: 300px;
+  max-height: 100px;
+  height: auto;
+  margin-left: 30px;
+  margin-right: 30px;
 }
 
 .attribution-row {
   display: flex;
   justify-content: center;
+  padding-bottom: 0.875rem;
   /* display: inline-block; */
   /* margin: 0 40px 0 40px; */
   /* align-self: center; */
@@ -99,8 +89,8 @@ a:link {
 }
 
 .foot-container {
-  margin-left: 0;
-  margin-right: 0;
+  margin-left: 0 !important;
+  margin-right: 0 !important;
   position: relative;
   display: flex;
   flex-direction: row;
@@ -116,6 +106,22 @@ a:link {
   height: auto !important;
   /* min-height: 120px; */
   padding-left: 1.5rem !important;
+  margin: 0 0 0 0 !important;
+}
+
+.cdh-logo {
+  text-align: left;
+  font-weight: 100;
+  font-style: normal;
+  font-size: 270%;
+  line-height: 0.8;
+  font-family: "Barlow Condensed", sans-serif;
+}
+
+.cdh-logo .cdh-logo-cursive {
+  font-family: "Clicker Script", cursive;
+  font-size: 100%;
+  vertical-align: 40%;
 }
 
 .foot .cdh-logo {
@@ -123,8 +129,21 @@ a:link {
 }
 
 @media screen and (max-width: 600px) {
-  .foot .cdh-logo {
-    display: none;
+  .contacts {
+    margin: auto auto;
   }
+
+  .foot .cdh-logo {
+    /* display: none; */
+  }
+
+}
+
+
+@media screen and (max-width: 1500px) {
+  .foot .cdh-logo {
+    /* display: none; */
+  }
+
 }
 </style>
